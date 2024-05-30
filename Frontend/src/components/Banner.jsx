fa7b7acb1228a0d4d4d620f0c521bd19e92e761e
jedidia-nku <jedinkuze123@gmail.com>
2024-05-30 17:39:22 +0200

    New updates

--- Frontend/src/components/Banner.jsx
+++ Frontend/src/components/Banner.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import banner from "../../public/Banner.png";
+import banner from "../../public/banner.png";
+import Login from "./Login";
 function Banner() {
   return (
     <>
-      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4 flex flex-col md:flex-row my-10">
+      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4 flex flex-col md:flex-row my-10">
+        
         <div className="w-full order-2 md:order-1 md:w-1/2 mt-12 md:mt-36">
           <div className="space-y-8">
             <h1 className="text-2xl md:text-4xl font-bold">
-              Hello, welcomes here to learn something{" "}
-              <span className="text-pink-500">new everyday!!!</span>
+              Hello, welcomes to the World of readers, {" "}
+              <span className="text-pink-500">Amazing storybooks found only here!!!</span>
             </h1>
             <p className="text-sm md:text-xl">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor,
-              et totam. Tempora amet atque expedita, quae corrupti totam sed
-              pariatur corporis at veniam est voluptas animi!
+            "Reading is a conversation. All books talk. But a good book listens as well."  — Mark Haddon
             </p>
             <label className="input input-bordered flex items-center gap-2">
               <svg
@@ -28,7 +28,17 @@ function Banner() {
               <input type="text" className="grow" placeholder="Email" />
             </label>
           </div>
-          <button className="btn mt-6 btn-secondary">Get Started</button>
+            <div className="">
+                <button
+                  className="btn mt-6 btn-secondary hover:bg-slate-400 duration-300 cursor-pointer"
+                  onClick={() =>
+                    document.getElementById("my_modal_3").showModal()
+                  }
+                >
+                  Get Started
+                </button>
+                <Login />
+                </div>
         </div>
         <div className=" order-1 w-full mt-20 md:w-1/2">
           <img
@@ -38,6 +48,7 @@ function Banner() {
           />
         </div>
       </div>
+      
     </>
   );
 }
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -3,7 +3,11 @@ export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     darkMode: "class",
     theme: {
-        extend: {},
+        extend: {
+            backgroundImage: {
+                'background': "url('../../public/Banner.jpg')",
+              }
+        },
     },
     plugins: [require("daisyui")],
 };
